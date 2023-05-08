c936eb2c78e6f68ffc37754c7e2e3f9e80c8f425
Xalvitor <68449900+Xalvitor@users.noreply.github.com>
2023-05-07 21:09:08 -0300

    Create routes.js

---
+++ src/router/routes.js
@@ -0,0 +1,56 @@
+/* eslint-disable linebreak-style */
+const routes = [
+
+  {
+    path: '/login',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', name: 'Login-usuario', component: () => import('pages/Login.vue') },
+    ],
+  },
+
+  {
+    path: '/inquilino',
+    component: () => import('layouts/InquilinoAutenticado.vue'),
+    children: [
+      { path: 'index', name: 'inquilino', component: () => import('src/pages/Inquilino/index.vue') },
+      { path: 'encomendas', name: 'Encomendas-inquilino', component: () => import('src/pages/Encomendas.vue') },
+    ],
+  },
+  {
+    path: '/porteiro',
+    component: () => import('layouts/PorteiroAutenticado.vue'),
+    children: [
+      { path: 'index', name: 'porteiro', component: () => import('src/pages/Porteiro/index.vue') },
+      { path: 'encomendas', name: 'Encomendas-porteiro', component: () => import('src/pages/Encomendas.vue') },
+      { path: 'apartamentos', name: 'Apartamentos-porteiro', component: () => import('src/pages/Apartamentos.vue') },
+      { path: 'usuarios', name: 'Usuarios-porteiro', component: () => import('src/pages/Usuarios.vue') },
+    ],
+  },
+  {
+    path: '/sindico',
+    component: () => import('layouts/SindicoAutenticado.vue'),
+    children: [
+      { path: 'index', name: 'sindico', component: () => import('src/pages/Sindico/index.vue') },
+      { path: 'encomendas', name: 'Encomendas-sindico', component: () => import('src/pages/Encomendas.vue') },
+      { path: 'apartamentos', name: 'Apartamentos-sindico', component: () => import('src/pages/Apartamentos.vue') },
+      { path: 'usuarios', name: 'Usuarios-sindico', component: () => import('src/pages/Usuarios.vue') },
+    ],
+  },
+  {
+    path: '/cadastrarEncomendas',
+    component: () => import('layouts/CadastrarEncomendas.vue'),
+    children: [
+      { path: '', name: 'CadastroEncomendas-sindico', component: () => import('src/pages/CadastrarEncomendas.vue') },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
